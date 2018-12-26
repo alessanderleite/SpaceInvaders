37853340d740c37748811fb11e688f735ae94484
alessanderleite <alessander.logistica@gmail.com>
2018-12-25 23:50:22 -0200

    initial commit

---
+++ app/src/main/java/br/com/alessanderleite/spaceinvaders/SpaceInvadersActivity.java
@@ -0,0 +1,12 @@
+package br.com.alessanderleite.spaceinvaders;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpaceInvadersActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ gradlew.bat
@@ -0,0 +1,45 @@
+<application>
+  <component name="ActionsCollector">
+    <counts>
+      <entry action="$Copy" count="3" />
+      <entry action="$Cut" count="1" />
+      <entry action="$Delete" count="10" />
+      <entry action="$Paste" count="3" />
+      <entry action="$Undo" count="315" />
+      <entry action="Android.BuildApk" count="1" />
+      <entry action="Android.BuildBundle" count="1" />
+      <entry action="Android.ChooseProfileConfiguration" count="1" />
+      <entry action="Android.SyncProject" count="8" />
+      <entry action="ChangeSignature" count="1" />
+      <entry action="CleanGradleProject" count="7" />
+      <entry action="CloseAllEditors" count="38" />
+      <entry action="CloseContent" count="3" />
+      <entry action="CloseProject" count="69" />
+      <entry action="CodeCleanup" count="1" />
+      <entry action="CodeCompletion" count="98" />
+      <entry action="CommentByBlockComment" count="3" />
+      <entry action="CommentByLineComment" count="41" />
+      <entry action="DialogOkAction Enter" count="31" />
+      <entry action="EditorBackSpace" count="7492" />
+      <entry action="EditorChooseLookupItem" count="7065" />
+      <entry action="EditorChooseLookupItemReplace" count="2" />
+      <entry action="EditorCodeBlockStart" count="1" />
+      <entry action="EditorCopy" count="207" />
+      <entry action="EditorCut" count="52" />
+      <entry action="EditorDelete" count="474" />
+      <entry action="EditorDown" count="10627" />
+      <entry action="EditorDownWithSelection" count="574" />
+      <entry action="EditorEnter" count="4030" />
+      <entry action="EditorEscape" count="156" />
+      <entry action="EditorIndentSelection" count="2" />
+      <entry action="EditorLeft" count="5617" />
+      <entry action="EditorLeftWithSelection" count="1609" />
+      <entry action="EditorLineEnd" count="382" />
+      <entry action="EditorLineEndWithSelection" count="154" />
+      <entry action="EditorLineStart" count="49" />
+      <entry action="EditorLineStartWithSelection" count="141" />
+      <entry action="EditorPageDown" count="32" />
+      <entry action="EditorPageDownWithSelection" count="1" />
+      <entry action="EditorPageUp" count="56" />
+      <entry action="EditorPaste" count="409" />
+      <entry action="Edito
