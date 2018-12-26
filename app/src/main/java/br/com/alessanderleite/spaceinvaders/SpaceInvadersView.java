9b3f22e4955be01cf460e642e2a49076142d43af
alessanderleite <alessander.logistica@gmail.com>
2018-12-26 18:38:23 -0200

    declaring all the variables in the SpaceInvadersView class

--- app/src/main/java/br/com/alessanderleite/spaceinvaders/SpaceInvadersView.java
+++ app/src/main/java/br/com/alessanderleite/spaceinvaders/SpaceInvadersView.java
@@ -1,4 +1,91 @@
 package br.com.alessanderleite.spaceinvaders;
 
-public class SpaceInvadersView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.media.SoundPool;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class SpaceInvadersView extends SurfaceView implements Runnable {
+
+    Context context;
+
+    //This is our thread
+    private Thread gameThread = null;
+
+    // Our SurfaceHolder to lock the surface before we draw our graphics
+    private SurfaceHolder ourHolder;
+
+    // A boolean which we will set and unset
+    // when the game is running- or not.
+    private volatile boolean playing;
+
+    // Game is paused at the start
+    private boolean paused = true;
+
+    // A Canvas and a Paint object
+    private Canvas canvas;
+    private Paint paint;
+
+    // This variable tracks the game frame rate
+    private long fps;
+
+    // This is used to help calculate the fps
+    private long timeThisFrame;
+
+    // The size of the screen in pixels
+    private int screenX;
+    private int screenY;
+
+    // The players ship
+    private PlayerShip playerShip;
+
+    // The player's bullet
+    private Bullet bullet;
+
+    // The invaders bullets
+    private Bullet[] invadersBullets = new Bullet[200];
+    private int nextBullet;
+    private int maxInvaderBullets = 10;
+
+    // Up to 60 invaders
+    Invader[] invaders = new Invader[60];
+    int numInvaders = 0;
+
+    // The player's shelters are built from bricks
+    private DefenceBrick[] bricks = new DefenceBrick[400];
+    private int numBricks;
+
+    // For sound FX
+    private SoundPool soundPool;
+    private int playerExplodeID = -1;
+    private int invaderExplodeID = -1;
+    private int shootID = -1;
+    private int damageShelterID = -1;
+    private int uhID = -1;
+    private int ohID = -1;
+
+    // The score
+    int score = 0;
+
+    // Lives
+    private int lives = 3;
+
+    // How menacing should the sound be?
+    private long menaceInterval = 1000;
+    // Which menace sound should play next
+    private boolean uhOrOh;
+    // When did we last play a menacing sound
+    private long lastMenaceTime = System.currentTimeMillis();
+
+
+    public SpaceInvadersView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
