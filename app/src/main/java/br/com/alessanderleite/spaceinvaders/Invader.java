ea700d8f68565e83b18c99091f668a6940d6ec65
alessanderleite <alessander.logistica@gmail.com>
2018-12-27 16:05:40 -0200

    declare all the objects and variables the Invader class will need

--- app/src/main/java/br/com/alessanderleite/spaceinvaders/Invader.java
+++ app/src/main/java/br/com/alessanderleite/spaceinvaders/Invader.java
@@ -1,4 +1,39 @@
 package br.com.alessanderleite.spaceinvaders;
 
+import android.graphics.Bitmap;
+import android.graphics.RectF;
+
+import java.util.Random;
+
 public class Invader {
+
+    RectF rect;
+
+    Random generator = new Random();
+
+    // The player ship will be represented by a Bitmap
+    private Bitmap bitmap1;
+    private Bitmap bitmap2;
+
+    // How long and high our invader will be
+    private float length;
+    private float height;
+
+    // X is the far left of the rectangle which forms our invader
+    private float x;
+
+    // Y is the top coordinate
+    private float y;
+
+    // This will hold the pixels per second speedthat the invader will move
+    private float shipSpeed;
+
+    public final int LEFT = 1;
+    public final int RIGHT = 2;
+
+    // Is the ship moving and in which direction
+    private int shipMoving = RIGHT;
+
+    boolean isVisible;
+
 }
