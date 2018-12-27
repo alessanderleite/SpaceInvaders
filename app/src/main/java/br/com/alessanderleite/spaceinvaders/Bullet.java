f72befcd760a4b3d92135b9ae622d348486e1fd9
alessanderleite <alessander.logistica@gmail.com>
2018-12-27 12:35:57 -0200

    add some variables, initialize the RectF object and call Bullet constructor in the Bullet class

--- app/src/main/java/br/com/alessanderleite/spaceinvaders/Bullet.java
+++ app/src/main/java/br/com/alessanderleite/spaceinvaders/Bullet.java
@@ -1,4 +1,32 @@
 package br.com.alessanderleite.spaceinvaders;
 
+import android.graphics.RectF;
+
 public class Bullet {
+
+    private float x;
+    private float y;
+
+    private RectF rect;
+
+    // Which way is it shooting
+    public final int UP = 0;
+    public final int DOWN = 1;
+
+    // Going nowhere
+    int heading = -1;
+    float speed = 350;
+
+    private int width = 1;
+    private int height;
+
+    private boolean isActive;
+
+    public Bullet(int screenY) {
+
+        height = screenY / 20;
+        isActive = false;
+
+        rect = new RectF();
+    }
 }
